0e60b81e71de33c9a701186613b01abc7a94ff70
ayotidapo <otidapson@gmail.com>
2024-12-14 06:36:03 +0100

    temporal fix

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
 	env: {
 		NEXT_PUBLIC_GOOGLE_PLACE_API: process.env.NEXT_PUBLIC_GOOGLE_PLACE_API,
 		NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
 		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 	},
 };
