8a6a3398f2c129bc26baaab237fafe12ff411231
ayotidapo <otidapson@gmail.com>
2024-12-14 06:34:57 +0100

    temporal fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,11 @@ const nextConfig = {
 	experimental: {
 		missingSuspenseWithCSRBailout: false,
 	},
-	env:{
-		NEXT_PUBLIC_GOOGLE_PLACE_API:process.env.NEXT_PUBLIC_GOOGLE_PLACE_API,
-        NEXT_PUBLIC_API_BASE_URL:process.env.NEXT_PUBLIC_API_BASE_URL,
-        NEXTAUTH_SECRET:'process.env.NEXTAUTH_SECRE',
-        NEXTAUTH_URL:process.env.NEXTAUTH_URL
-	}
+	env: {
+		NEXT_PUBLIC_GOOGLE_PLACE_API: process.env.NEXT_PUBLIC_GOOGLE_PLACE_API,
+		NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+	},
 };
 
 export default nextConfig;
