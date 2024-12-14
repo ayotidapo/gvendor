98ad03b88b5d9b24e5afe48584b9495fbe971c43
ayotidapo <otidapson@gmail.com>
2024-12-14 06:40:24 +0100

    remove console log

--- src/app/(user)/midd1eware.ts
+++ src/app/(user)/midd1eware.ts
@@ -3,8 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function middleware(request: NextRequest) {
 	// Get session
 	// const session = await getServerSession(NextAuthOptions);
-	// console.log({ session, i: 0 });
-	// Check if session exists
+
 	//if (!session) {
 
 	return NextResponse.redirect(new URL('/auth/login', request.url));
