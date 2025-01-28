38adb68fd24529edfb98573f18266e86f7de9ff1
ayotidapo <otidapson@gmail.com>
2025-01-28 17:23:37 +0100

    orders page responsiveness

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -26,7 +26,7 @@ const UserLayout: React.FC<{ children: React.ReactNode }> = async props => {
 	return (
 		<div className='flex'>
 			<Sidebar />
-			<div className='flex-1 p-8 px-12 h-[calc(100vh-64px)] overflow-y-auto'>
+			<div className='flex-1 md:p-8 md:px-12 xx:p-2.5 h-[calc(100vh-64px)] overflow-y-auto'>
 				<Suspense>
 					<GetUserLayout vendor={{ ...vendor, goodToken }}>
 						{props.children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -41,6 +41,7 @@ module.exports = {
 			},
 			screens: {
 				xs: '500px',
+				xc: '425px',
 				xx: '320px',
 			},
 		},
